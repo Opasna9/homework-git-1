c5e9336e574eaf04275cae13785af43ab5799737
romanovskaya_t <Mainnt90>
2019-09-25 11:23:41 +0300

    clean work

--- src/work/Test.java
+++ src/work/Test.java
@@ -4,11 +4,7 @@ public class Test implements Cloneable{
     public int i;
 
     public static void main(String[] args) {
-        //первый случай
-        int a = 300;
-        //второй случай
-        Integer b = 301;
-
+       
     }
 
 }
