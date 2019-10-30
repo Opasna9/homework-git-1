169f49b5d8e06848772c0558fa9fa0a87b0dede3
romanovskaya_t <Mainnt90>
2019-10-30 16:32:15 +0300

    test generic

--- src/conspect/oop/generic/GenericExtends.java
+++ src/conspect/oop/generic/GenericExtends.java
@@ -3,16 +3,34 @@ package conspect.oop.generic;
 public class GenericExtends {
     public static void main(String[] args) {
 
+        Box box1 = new Box(1);
+        Box box2 = new Box(3L);
+        Number s = sumBox(box1,box2);
+        System.out.println("box1 + box2 = " + s);
+
+        System.out.println(sum(3,6L));
+
     }
 
-    public Number sum(Box<? extends Number> b1,Box<? extends Number> b2){
+    // Используем дженерик класс в методе
+    public static Number sumBox(Box<? extends Number> b1,Box<? extends Number> b2){
         Number result = b1.getValue().doubleValue() + b2.getValue().doubleValue();
+        return result;
+    }
+
+    // Дженерилизируем только метод
+    public static <T extends String, K extends Number, M extends Number> T sum(K value1, M value2){
+        T result = (T) Double.toString(value1.doubleValue() + value2.doubleValue());
         return result;
     }
 }
 
 class Box <T extends Number>{
     private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
 
     public T getValue() {
         return value;
