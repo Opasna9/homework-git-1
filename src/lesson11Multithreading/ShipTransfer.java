6c3ceac00ce148d0daff32b03cb2ff9915f0cfa0
romanovskaya_t <Mainnt90>
2019-11-01 16:54:55 +0300

    add homework Multithreading

---
+++ src/lesson11Multithreading/ShipTransfer.java
@@ -0,0 +1,109 @@
+package lesson11Multithreading;
+
+import javax.sound.sampled.Port;
+import java.util.concurrent.*;
+
+public class ShipTransfer implements Callable<Cargo> {
+    private int id;
+    private Cargo cargo;
+    private CountDownLatch countDownLatch;
+    private ConcurrentLinkedQueue<ShipTransfer> queueShip;
+    private ConcurrentLinkedQueue<Berth> queueBerth;
+
+    public ShipTransfer() {
+    }
+
+    public ShipTransfer(int id, Cargo cargo, CountDownLatch countDownLatch, ConcurrentLinkedQueue<ShipTransfer> queueShip, ConcurrentLinkedQueue<Berth> queueBerth) {
+        this.id = id;
+        this.cargo = cargo;
+        this.countDownLatch = countDownLatch;
+        this.queueShip = queueShip;
+        this.queueBerth = queueBerth;
+    }
+
+    @Override
+    public Cargo call() {
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return new Cargo("error1:" + e.getMessage());
+        }
+        System.out.println("Ship " + id + " started. " + cargo);
+
+
+        // проходим через очередь
+        queueShip.add(this);
+        //queueShip.put(this);
+        System.out.println("Ship " + id + " in queueShip. ");
+
+
+        while (true) {
+            // проверяем первые ли мы в очереди
+            if (queueShip.peek() == this) {
+                Berth b = queueBerth.peek();
+                assert b != null;
+                if (b.occupiedPlaces.get() < 2) {
+
+                    b.BARRIER.getParties();
+
+                    // пришли в первый свободный причал (где есть хотя бы 1 место свободное)
+                    // занимаем место в причале
+                    b.occupiedPlaces.incrementAndGet();
+                    // ждем некст
+
+                    // если все места(2) заняты перемещаем причал в конец очереди
+                    if (b.occupiedPlaces.get() == 2) {
+                        queueBerth.poll();
+                        queueBerth.offer(b);
+                    }
+
+                    queueShip.poll();// выходим из очереди
+
+                    System.out.println("Places = " + b.occupiedPlaces.get());
+                    System.out.println("Ship " + id + " -----> Berth " + b.getId());
+
+                   // ждем 2-ого для обмена
+                    try {
+                        b.BARRIER.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                        return new Cargo("error2:" + e.getMessage());
+                    }
+
+                    try {
+                        cargo = b.EXCHANGER.exchange(cargo);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return new Cargo("error3:" + e.getMessage());
+                    }
+                    // запишем на посылке, в каком причале обменялись
+                    cargo.setBerth(b.getId());
+                    System.out.println("Дождались и обменялись");
+
+                    //Thread.sleep(1000);
+                    // освобождаем место
+                    b.occupiedPlaces.decrementAndGet();
+
+                    System.out.println("Ship " + id + " end. " + cargo);
+                    break;
+                }
+            } else {
+                // пусть другие пока займут процессорное время
+                Thread.yield();
+                //Thread.sleep(100);
+            }
+        }
+
+
+        //Berth berth = queueBerth.take();
+
+
+        //System.out.println(queueBerth.take());
+
+        return cargo;
+    }
+}
